3be8a383f986797723e71e5aa9d04c0983a7ce79
Rakha Viantoni <Arkan@Arkans-MacBook-Pro.local>
2020-02-10 09:58:48 +0700

    add crud

--- src/api/car.js
+++ src/api/car.js
@@ -6,7 +6,7 @@ export default ({ url, config, defaultParams }) => {
     list: () => axios.get(`${url}${endpoint}`, config()),
     status: (data, params) => axios.get(`${url}${endpoint}status/${data.date}`, { ...config(), params: { ...defaultParams, ...params } }),
     add: data => axios.post(`${url}${endpoint}`, data, config()),
-    update: data => axios.put(`${url}${endpoint}`, data, config()),
-    delete: data => axios.delete(`${url}${endpoint}`, data, config())
+    update: data => axios.put(`${url}${endpoint}${data.old_registration_no}`, data, config()),
+    delete: data => axios.delete(`${url}${endpoint}${data.registration_no}`, config())
   }
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,10 @@ import 'react-day-picker/lib/style.css';
 import { CarApi, ReservationApi } from '../../api';
 import { dateOnly } from '../../utils/formatter/datetime';
 import { Icon } from '@iconify/react';
+import add from '@iconify/icons-ant-design/appstore-add-outlined';
 import close from '@iconify/icons-ant-design/close-circle-outlined';
+import editIcon from '@iconify/icons-ant-design/edit-twotone';
+import deleteIcon from '@iconify/icons-ant-design/delete-twotone';
 
 class Home extends React.Component {
   timeoutSearch = undefined;
@@ -23,10 +26,17 @@ class Home extends React.Component {
         customer: '',
         date: ''
       },
+      carform: {
+        registration_no: '',
+        color: ''
+      },
       selectedDay: undefined,
       isLoading: true,
       visiblePopup: false,
-      visibleConfirm: false
+      visibleConfirm: false,
+      visibleAdd: false,
+      visibleEdit: false,
+      visibleDelete: false
     };
   }
 
@@ -81,9 +91,10 @@ class Home extends React.Component {
   }
 
   onChange = (e, fname) => {
-    let { form } = this.state
+    let { form, carform } = this.state
     form[fname] = e.target.value
-    this.setState({ form })
+    carform[fname] = e.target.value
+    this.setState({ form, carform })
   }
 
   handleSubmit = (e) => {
@@ -97,7 +108,7 @@ class Home extends React.Component {
         Toast.hide()
         Toast.success(response.data.message, 2, null, true)
         this.fetchCarList(selectedDay ? dateOnly(selectedDay) : dateOnly(new Date()))
-        this.setState({ visiblePopup: false, form: { registration: '', customer: '', date: '' } })
+        this.setState({ visiblePopup: false, form: { registration_no: '', customer: '', date: '' } })
       }
     }).catch(err => {
       Toast.hide()
@@ -125,9 +136,67 @@ class Home extends React.Component {
     })
   }
 
+  handleAdd = (e) => {
+    e.preventDefault()
+    this.setState({ loading: true })
+    Toast.loading('Loading...', 1000, null, true)
+    let { carform, selectedDay } = this.state
+    CarApi.add(carform)
+    .then(response => {
+      if(response.data.status === 200){
+        Toast.hide()
+        Toast.success(response.data.message, 2, null, true)
+        this.fetchCarList(selectedDay ? dateOnly(selectedDay) : dateOnly(new Date()))
+        this.setState({ visibleAdd: false, carform: { registration_no: '', color: '' } })
+      }
+    }).catch(err => {
+      Toast.hide()
+      Toast.fail(err.response?err.response.data.message:JSON.stringify(err.message), 3, null, true)
+    })
+  }
+
+  handleEdit = (e) => {
+    e.preventDefault()
+    this.setState({ loading: true })
+    Toast.loading('Loading...', 1000, null, true)
+    let { data, carform, selectedDay } = this.state
+    let form = { old_registration_no: data.registration_no, ...carform }
+    CarApi.update(form)
+    .then(response => {
+      if(response.data.status === 200){
+        Toast.hide()
+        Toast.success(response.data.message, 2, null, true)
+        this.fetchCarList(selectedDay ? dateOnly(selectedDay) : dateOnly(new Date()))
+        this.setState({ visibleEdit: false, carform: { registration_no: '', color: '' } })
+      }
+    }).catch(err => {
+      Toast.hide()
+      Toast.fail(err.response?err.response.data.message:JSON.stringify(err.message), 3, null, true)
+    })
+  }
+  handleDelete = (e) => {
+    e.preventDefault()
+    this.setState({ loading: true })
+    Toast.loading('Loading...', 1000, null, true)
+    let { data, carform, selectedDay } = this.state
+    let form = { ...data, ...carform }
+    CarApi.delete(form)
+    .then(response => {
+      if(response.data.status === 200){
+        Toast.hide()
+        Toast.success(response.data.message, 2, null, true)
+        this.fetchCarList(selectedDay ? dateOnly(selectedDay) : dateOnly(new Date()))
+        this.setState({ visibleDelete: false, carform: { registration_no: '', color: '' } })
+      }
+    }).catch(err => {
+      Toast.hide()
+      Toast.fail(err.response?err.response.data.message:JSON.stringify(err.message), 3, null, true)
+    })
+  }
+
   render() {
 
-    let { data, list, form, selectedDay, visiblePopup, visibleConfirm } = this.state
+    let { data, list, form, carform, selectedDay, visiblePopup, visibleConfirm, visibleAdd, visibleEdit, visibleDelete } = this.state
     
     const handleTogglePopup = (value, data) => {
       this.setState({ visiblePopup: value, data, form: { registration_no: data.registration_no ? data.registration_no : '', customer: '', date: selectedDay ? dateOnly(selectedDay) : dateOnly(new Date()) } })
@@ -135,6 +204,16 @@ class Home extends React.Component {
     
     const handleToggleConfirm = (value, data) => {
       this.setState({ visibleConfirm: value, data, selectedDay })
+    }
+    
+    const handleToggleAdd = (value) => {
+      this.setState({ visibleAdd: value, carform: { registration_no: '', color: '' } })
+    }
+    const handleToggleEdit = (value, data) => {
+      this.setState({ visibleEdit: value, data, carform: { registration_no: data.registration_no ? data.registration_no : '', color: data.color ? data.color : '' } })
+    }
+    const handleToggleDelete = (value, data) => {
+      this.setState({ visibleDelete: value, data, carform: { registration_no: data.registration_no ? data.registration_no : '', color: data.color ? data.color : '' } })
     }
     // const handleSignout = e => {
     //   BACK_TO_LOGIN(true)
@@ -148,11 +227,11 @@ class Home extends React.Component {
               <div className="title">
                 Car Rental
               </div>
-              {/* <div className="profile" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <button className="btn-menu" onClick={ handleSignout } style={{ paddingRight: 0, marginLeft: 5 }}>
-                  <Icon icon={signOut} color="white" />
+              <div className="profile" style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <button className="btn-menu" onClick={ handleToggleAdd } style={{ paddingRight: 0, marginLeft: 5 }}>
+                  <Icon icon={add} color="white" heihgt="30" width="30" />
                 </button>
-              </div> */}
+              </div>
             </div>
             <div style={{ marginTop: 30, marginBottom: -20, textAlign: 'center', color: 'white' }}>
               <DayPicker onDayClick={this.fetchCarList} selectedDays={this.selectedDay} />
@@ -164,6 +243,10 @@ class Home extends React.Component {
           { 
             list.slice(0).reverse().map((item, key) => 
               <div className="body" key={ key }>
+                <div className="action">
+                  <Icon icon={editIcon} color="white" heihgt="30" width="30" onClick={ e => handleToggleEdit(true, item) } />
+                  <Icon icon={deleteIcon} color="white" heihgt="30" width="30" onClick={ e => handleToggleDelete(true, item) } />
+                </div>
                 <div className="title">
                   { item.registration_no }&nbsp;<div className="circle" title={item.color} style={{backgroundColor:item.color}}></div>
                 </div>
@@ -223,6 +306,71 @@ class Home extends React.Component {
               No
             </button>
             <button className="btn-confirmation--outline" onClick={ e => this.handleCancel(e) }>
+              Yes
+            </button>
+          </div>
+        </div>
+
+        <div className={ visibleAdd ? "popup active" : "popup" } >
+          <div className="content">
+            { visibleAdd && 
+            <button className="btn-close" onClick={ e => handleToggleAdd(false) }>
+              <Icon icon={close} color="white" width="30" height="30" />
+            </button>
+            }
+            <div className="title">Add new car</div>
+            <form onSubmit={ this.handleAdd } autoComplete="off">
+              <div className="form">
+                <div className="input-container">
+                <input type="text" className="custom-field" placeholder="Registration No" onChange={ e => this.onChange(e, 'registration_no') } />
+                </div>
+                <div className="input-container">
+                <input type="text" className="custom-field" placeholder="Color" onChange={ e => this.onChange(e, 'color') } />
+                </div>
+              </div>
+              <button className={ (carform.registration_no&&carform.color) !== '' ? 'btn-submit' : 'btn-submit disabled' } type="submit">
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className={ visibleEdit ? "popup active" : "popup" } >
+          <div className="content">
+            { visibleEdit && 
+            <button className="btn-close" onClick={ e => handleToggleEdit(false, {}) }>
+              <Icon icon={close} color="white" width="30" height="30" />
+            </button>
+            }
+            <div className="title">Edit {data.registration_no}</div>
+            <form onSubmit={ this.handleEdit } autoComplete="off">
+              <div className="form">
+                <div className="input-container">
+                <input type="text" className="custom-field" placeholder="Registration No" onChange={ e => this.onChange(e, 'registration_no') } value={carform.registration_no} />
+                </div>
+                <div className="input-container">
+                <input type="text" className="custom-field" placeholder="Color" onChange={ e => this.onChange(e, 'color') } value={carform.color} />
+                </div>
+              </div>
+              <button className={ (carform.registration_no&&carform.color) !== '' ? 'btn-submit' : 'btn-submit disabled' } type="submit">
+                Update
+              </button>
+            </form>
+          </div>
+        </div>
+
+        <div className={ visibleDelete ? "popup active" : "popup" } >
+          <div className="content">
+            { visibleDelete && 
+            <button className="btn-close" onClick={ e => handleToggleDelete(false, {}) }>
+              <Icon icon={close} color="white" width="30" height="30" />
+            </button>
+            }
+            <div className="title">Delete {data.registration_no}?</div>
+            <button className="btn-confirmation" onClick={ e => handleToggleDelete(false, {}) }>
+              No
+            </button>
+            <button className="btn-confirmation--outline" onClick={ e => this.handleDelete(e) }>
               Yes
             </button>
           </div>
